b3faa18e994f65d3d618a5ea5d58b0efb3571b04
emabi16 <emabi16@hotmail.com>
2017-02-24 12:38:34 +0100

    The first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+ var express=require('express');
+ var app = express();
+
+ app.use('/',function(req,res){
+   res.sendFile('index.html',{root:__dirname +'/'});
+ });
+
+ app.listen(4000,function(){
+   console.log('Its running at port 4000')
+ })
+
+
+
+// get the module
+
+// instantiate the module to get the oblect
+
+// use the objet to send and req aand response
