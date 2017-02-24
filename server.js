cc1f251bbb7d7290f45c6064dad04fbe4c9a8e7e
emabi16 <emabi16@hotmail.com>
2017-02-24 13:26:16 +0100

    adde to my site

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
  var express=require('express');
  var app = express();
 
- app.use('/',function(req,res){
-   res.sendFile('index.html',{root:__dirname +'/'});
- });
+// app.use('/',function(req,res){
+   //res.sendFile('index.html',{root:__dirname +'/'});
+ //});
+  app.use(express.static(__dirname + '/'));
   app.set('port',process.env.PORT||5000)
- app.listen(app.get('port'),function(){
+  app.listen(app.get('port'),function(){
    console.log('Its running at port' +app.get('port'))
  })
 
